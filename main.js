e91bf9a6cb3349e49ff516334572cc8815fb2842
Michee_Tokatoka <tokatokakiyombo@gmail.com>
2022-12-25 16:01:05 +0200

    Spicing things up by learning about modal while adding it on the base code. Have change a few other things as well...

---
+++ main.js
@@ -0,0 +1,28 @@
+//Start adding profile-pic modal to the base program
+let modal = document.getElementById("prof-pic-modal");
+let profilePic = document.getElementById("profile-pic");
+let profPicModalCloseBtn = document.getElementById("prof-pic-modal-close-btn");
+
+profilePic.addEventListener("click", openModal);
+profPicModalCloseBtn.addEventListener("click", closeModal);
+window.addEventListener("click",outsideClick);
+
+function openModal(){
+    modal.style.display = "flex";
+}
+
+function closeModal(){
+    modal.style.display = "none";
+}
+
+//Uncommenting the folowwing function will allow to close the modal by clicking
+//anywhere outside the modal-picture.
+/*
+function outsideClick(windowElement){
+if(windowElement.target == modal){
+    modal.style.display = "none";
+}
+}
+*/
+
+//End adding profile-pic modal to the base program
